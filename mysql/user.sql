0d420b59b29465bb56470fc381e5e78ed07d9f9e
tujiawei <you@example.com>
2021-07-03 21:50:22 +0800

    feat(mysql):add mysql

--- mysql/user.sql
+++ mysql/user.sql
@@ -1,5 +1,16 @@
+# 删除表
+drop table if exists user;
+
 create table user(
-  id int primary key auto_crement,
-  name varchar(20),
+  id int(20),
+  name varchar(40),
   age int
-);
+) default charset=utf8mb4;
+
+# 显示表字段
+desc user;
+explain user;
+show columns from user;
+
+# 显示建表语句
+show create table user;

0a095c1c6f75e44b4007b2f58c9d626e23bafe3e
tujiawei <you@example.com>
2022-06-03 21:10:55 +0800

    feat(react): 1比1还原react源码

---
+++ react/README.md
@@ -0,0 +1,5 @@
+# react
+
+react 1比1还原react源码  
+
+## scheduler
---
+++ react/packages/scheduler/src/SchedulerFeatureFlags.ts
@@ -0,0 +1,14 @@
+/**
+ * 特性状态值
+ * @filename: packages/scheduler/src/SchedulerFeatureFlags.ts
+ * @author: Mr Prince
+ * @date: 2022-06-03 20:39:38
+ */
+
+export const enableSchedulerDebugging = false;
+export const enableIsInputPending = false;
+export const enableProfiling = false;
+export const enableIsInputPendingContinuous = false;
+export const frameYieldMs = 5;
+export const continuousYieldMs = 50;
+export const maxYieldMs = 300;

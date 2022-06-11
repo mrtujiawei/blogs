0635ee149baacca7d2b895ff2a41f5b9a7679c59
tujiawei <you@example.com>
2022-06-11 14:34:16 +0800

    feat(reactivity): 暂存

--- vue/packages/reactivity/src/index.ts
+++ vue/packages/reactivity/src/index.ts
@@ -7,8 +7,39 @@ logger.subscribe((content) => {
   console.log(content.getFormattedMessage());
 });
 
-logger.trace(isObject('a'));
+const reactiveMap = new WeakMap<Object, any>();
 
-logger.debug('debug');
+function track() {
+  logger.trace('track');
+}
 
-logger.info('Hello World');
+function trigger() {
+  logger.trace('trigger');
+}
+
+export function reactivity<T extends Object>(value: T): any {
+  if (!isObject(value)) {
+    return value;
+  }
+
+  if (reactiveMap.has(value)) {
+    return reactiveMap.get(value);
+  }
+
+  const proxy = new Proxy(value, {
+    get(target, key, receiver) {
+      track();
+      logger.trace(`get ${String(key)}`);
+      return reactivity(Reflect.get(target, key, receiver));
+    },
+    set(target, key, value, receiver) {
+      trigger();
+      logger.trace(`set ${String(key)} = ${value}`);
+      return Reflect.set(target, key, value, receiver);
+    },
+  });
+
+  reactiveMap.set(value, proxy);
+
+  return proxy;
+}

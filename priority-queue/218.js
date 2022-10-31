8219afc9c001855cceaa31e856d03ac66130b474
tujiawei <you@example.com>
2022-10-31 18:51:23 +0800

    refactor(blogs): 删除一些无效的代码

--- priority-queue/218.js
+++
@@ -1,9 +0,0 @@
-/**
- * 城市天际线
- * leetcode 218
- * @see {@link https://leetcode-cn.com/problems/the-skyline-problem/}
- * @filename: 218.js
- * @author: Mr Prince
- * @date: 2021-07-13 08:13:35
- */
-
--- rxjs/Observable.ts
+++
@@ -1,6 +0,0 @@
-class Observable {
-  constructor(private subscriber: any) {}
-  subscribe(observer: any) {
-    return this.subscriber(observer);
-  }
-}
